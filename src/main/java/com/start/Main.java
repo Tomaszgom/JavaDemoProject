e293e57cf7b4229a0aebbd8c3579a49abb04508d
Tomo <t.gomoradzki@gmail.com>
2019-06-02 20:28:39 +0200

    Refactoring

--- src/main/java/com/start/Main.java
+++ src/main/java/com/start/Main.java
@@ -14,7 +14,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-/*************************************************
+/************************************************
  * Launch Application Class
 *************************************************/	
 
